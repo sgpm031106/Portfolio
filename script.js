b92e578845aec9a2ce6b74bb5bb5d4db759e6caa
sgpm031106 <sergioprogr@gmail.com>
2024-09-05 12:35:43 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+document.getElementById('button1').addEventListener('click', function() {
+    window.location.href = 'general.html';
+});
+
+document.getElementById('button2').addEventListener('click', function() {
+    window.location.href = 'hobbies.html';
+});
+
+document.getElementById('button3').addEventListener('click', function() {
+    window.location.href = 'Proyects.html';
+});
+
+
+
---
+++ script2.js
@@ -0,0 +1,6 @@
+
+
+window.onload = function() {
+    document.querySelector('.image-side').classList.add('show');
+    document.querySelector('.text-side').classList.add('show');
+};
